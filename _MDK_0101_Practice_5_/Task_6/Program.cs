d639edcdeda4d0e363de15a3da7f1a5dd136640e
abobus1235345345 <139453779+abobus1235345345@users.noreply.github.com>
2024-04-04 17:32:32 +0300

    Add files via upload

---
+++ _MDK_0101_Practice_5_/Task_6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Task_6
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n;
+            Console.Write("Введите размерность массива: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            int [] a = new int[n];
+            int[] nul = new int[n];
+            Random rand = new Random();
+            Console.WriteLine("Элементы массива: ");
+            for ( int i = 0; i<n; ++i)
+            {
+                a[i] = (int)rand.Next(0,3);
+                Console.Write(" " + a[i]);
+            }
+            Console.WriteLine("\nЭлементы = 0 находится на позициях: ");
+            for (int i = 0; i<n; ++i)
+            {
+                if (a[i] == 0)
+                {
+                    nul[i] = i+1;
+                   
+                    Console.Write($" {nul[i]}");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
